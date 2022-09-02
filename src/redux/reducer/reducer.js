b004e3a829e83bf4a6f9930ebb2b566c2c701d88
chuhoon <khchu1220@naver.com>
2022-09-02 14:50:31 +0900

    Chore: 주석 제거

--- src/redux/reducer/reducer.js
+++ src/redux/reducer/reducer.js
@@ -6,7 +6,6 @@ function reducer(state = initialState, action) {
     const { type, payload } = action;
     switch (type) {
         case 'ADD_CONTACT':
-            // state 속 contactList를 유지하되 name이랑 phoneNumber를 넣어준다.
             return {
                 ...state,
                 contactList: [
